f17bf179ba1f77128a0157a4123f278952a869bd
Wisner Oliveira <wisner.oliveira@gmail.com>
2017-10-07 16:08:47 -0300

    Initial Commit

---
+++ src/main/java/com/opensanca/trilharest/filmes/FilmesApplication.java
@@ -0,0 +1,19 @@
+package com.opensanca.trilharest.filmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author wisner
+ *
+ */
+
+@EnableAutoConfiguration
+@ComponentScan
+public class FilmesApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FilmesApplication.class, args);
+	}
+}
---
+++ src/main/java/com/opensanca/trilharest/filmes/hello/HelloController.java
@@ -0,0 +1,21 @@
+package com.opensanca.trilharest.filmes.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wisner
+ *
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	
+	@RequestMapping(path="/getHello", method=RequestMethod.GET)	
+	public String helloWorld() {
+		return "hello world";	
+	}
+	
+}
