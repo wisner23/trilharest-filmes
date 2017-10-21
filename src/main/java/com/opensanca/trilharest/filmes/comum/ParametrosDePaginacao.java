1e57c06fb0af32485417960fa1ba01916a100cb6
Wisner Oliveira <wisner.oliveira@gmail.com>
2017-10-21 16:39:23 -0200

    Bugfix for API 500

--- src/main/java/com/opensanca/trilharest/filmes/comum/ParametrosDePaginacao.java
+++ src/main/java/com/opensanca/trilharest/filmes/comum/ParametrosDePaginacao.java
@@ -5,6 +5,11 @@ public class ParametrosDePaginacao {
 	private Integer pagina;
 	private Integer tamanhoDaPagina;
 	
+	public ParametrosDePaginacao() {
+		this.pagina = 0;
+		this.tamanhoDaPagina = 0;
+	}
+	
 	public Integer getPagina() {
 		return pagina;
 	}
